7d6d4f3f870d476e3cae476000c8a80e8b4cf98a
ruxchuk <ruxchuk@gmail.com>
2014-03-04 10:53:00 +0700

    add permission menu

--- application/views/sidebar_menu.php
+++ application/views/sidebar_menu.php
@@ -2,6 +2,55 @@
 $baseUrl = base_url();
 $webUrl = $this->Constant_model->webUrl();
 $objCheckModule = $this->Module_model->checkModuleByName();
+
+$checkShowISPOTMenu = false;
+$checkShowSettingMenu = false;
+$checkShowIssueMenu = false;
+foreach ($objCheckModule as $key => $value) {
+    $expResult = explode(',', $value->permission);
+    switch ($value->title) {
+        case "Company":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowISPOTMenu = true;
+            }
+            break;
+        case "Device":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowISPOTMenu = true;
+            }
+            break;
+        case "Users":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowSettingMenu = true;
+            }
+            break;
+        case "Departments":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowSettingMenu = true;
+            }
+            break;
+        case "Position":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowSettingMenu = true;
+            }
+            break;
+        case "Modules":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowSettingMenu = true;
+            }
+            break;
+        case "Log":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowSettingMenu = true;
+            }
+            break;
+        case "Issue":
+            if (@$expResult[0] || @$expResult[1] || @$expResult[4] || @$expResult[5] || @$expResult[6]) {
+                $checkShowIssueMenu = true;
+            }
+            break;
+    }
+}
 ?>
 <div id="left">
 <form action="#" method="GET" class='search-form'>
@@ -32,7 +81,7 @@ endforeach;
 ?>
 <!-- #### iSpot #### -->
 <div class="subnav subnav-hidden">
-    <div class="subnav-title">
+    <div class="subnav-title <?php echo $checkShowISPOTMenu ? "": 'hidden'; ?>">
         <a href="#" class='toggle-subnav'><i class="icon-angle-down"></i><span>iSpot</span></a>
     </div>
     <?php
@@ -101,7 +150,7 @@ endforeach;
 
 <!-- #### Settings #### -->
 <div class="subnav subnav-hidden">
-    <div class="subnav-title">
+    <div class="subnav-title <?php echo $checkShowSettingMenu ? "": 'hidden'; ?>">
         <a href="#" class='toggle-subnav'><i class="icon-angle-down"></i><span>Settings</span></a>
     </div>
     <?php
@@ -201,7 +250,7 @@ endforeach;
 
 <!-- #### Issue #### -->
 <div class="subnav subnav-hidden">
-    <div class="subnav-title">
+    <div class="subnav-title <?php echo $checkShowIssueMenu ? "": 'hidden'; ?>">
         <a href="#" class='toggle-subnav'><span>Issue</span></a>
     </div>
     <ul class="subnav-menu">
