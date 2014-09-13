27d8f7530c65d531a7846e6ffe2a8490c100bf0c
Rux <ruxchuk@gmail.com>
2014-09-13 23:42:48 +0700

    quotation print

--- application/views/quotation/print.php
+++ application/views/quotation/print.php
@@ -33,6 +33,10 @@ $countQuotationItem = count($objQuotationItem)
             font-size: 12px;
         }
 
+        table tr td {
+            padding: 3px;
+        }
+
         table .content {
             margin: 1em;
             border-collapse: collapse;
@@ -68,6 +72,15 @@ $countQuotationItem = count($objQuotationItem)
         .border-right {
             border-right: 1px #000 solid;
         }
+
+        .font-11 {
+            font-size: 11px !important;
+        }
+
+        table.padding2px tr td {
+            padding: 0px 2px 0px 2px;
+        !important;
+        }
     </style>
 </head>
 <body>
@@ -91,11 +104,8 @@ $countQuotationItem = count($objQuotationItem)
                                     <span class="style3"><b
                                             style="font-size: 10px;">                                </span>
 
-                                <div align="left" style="font-size:9px;">Specialist web designer, web application,
-                                    Hotspot
-                                    Wifi Solution, CCTV Camera. Specialist in
-                                    clean, standards compliant with a focus on simplicity, usability &
-                                    accessibility.
+                                <div align="left" style="font-size:9px;">We are confident of providing services to
+                                    support the goals. And solve all your business problems.
                                 </div>
                             </td>
                         </tr>
@@ -120,77 +130,81 @@ $countQuotationItem = count($objQuotationItem)
     <td>
         <table width="100%" cellpadding="0" cellspacing="0">
             <tr>
-                <td width="120" align="left" valign="top" class="border-top border-left">To :</td>
+                <td width="170" align="left" valign="top" class="border-top border-left">ชื่อลูกค้า / Customer :</td>
                 <td align="left" valign="top" class="border-top"> <?php echo @$contact_name; ?></td>
-                <td width="150" colspan="" align="left" valign="top" class="border-top border-left">เลขที่เอกสาร /
-                    Quotation No. :
+                <td width="150" colspan="" align="left" valign="top" class="border-top border-left font-11">เลขที่เอกสาร
+                    / Quotation No. :
                 </td>
                 <td width="150" align="left" valign="top"
                     class="border-top border-left border-right"><?php echo @$quotation_no; ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-left">ชื่อบริษัท / Crop. Name :</td>
+                <td align="left" valign="top" class="border-left font-11">ชื่อบริษัท / Crop. Name :</td>
                 <td align="left" valign="top"> <?php echo @$customer_name; ?></td>
-                <td align="left" valign="top" class="border-left">วันที่เสนอ / Quotation Date :</td>
+                <td align="left" valign="top" class="border-left font-11">วันที่เสนอ / Quotation Date :</td>
                 <td align="left" valign="top"
                     class="border-top border-left border-right"><?php echo $this->Constant_model->thaiDate(@$quotation_date, false, false); ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-left">ที่อยู่ / Address :</td>
+                <td align="left" valign="top" class="border-left font-11">ที่อยู่ / Address :</td>
                 <td align="left" valign="top"> <?php echo @$address; ?></td>
-                <td align="left" valign="top" class="border-left">ผู้ที่เสนอ / Proposer :</td>
+                <td align="left" valign="top" class="border-left font-11">ผู้ที่เสนอ / Proposer :</td>
                 <td align="left" valign="top" class="border-top border-left border-right"><?php echo @$proposer; ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-left">เบอร์มือถือ / Mobile :</td>
+                <td align="left" valign="top" class="border-left font-11">เบอร์มือถือ / Mobile :</td>
                 <td align="left" valign="top"> <?php echo @$contact_mobile; ?></td>
-                <td align="left" valign="top" class="border-left">เบอร์มือถือ / Mobile :</td>
+                <td align="left" valign="top" class="border-left font-11">เบอร์มือถือ / Mobile :</td>
                 <td align="left" valign="top" class="border-top border-left border-right"><?php echo @$mobile; ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-left">เบอร์ติดต่อ / Tel :</td>
+                <td align="left" valign="top" class="border-left font-11">เบอร์ติดต่อ / Tel :</td>
                 <td align="left" valign="top"> <?php echo @$contact_tel; ?></td>
-                <td align="left" valign="top" class="border-left">เบอร์ติดต่อ / Telephone :</td>
+                <td align="left" valign="top" class="border-left font-11">เบอร์ติดต่อ / Telephone :</td>
                 <td align="left" valign="top"
                     class="border-top border-left border-right"><?php echo @$telephone; ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-left">เบอร์แฟ็ก / Fax :</td>
+                <td align="left" valign="top" class="border-left font-11">เบอร์แฟ็ก / Fax :</td>
                 <td align="left" valign="top"> <?php echo @$contact_fax; ?></td>
-                <td align="left" valign="top" class="border-left">เบอร์แฟ็ก / Fax:</td>
+                <td align="left" valign="top" class="border-left font-11">เบอร์แฟ็ก / Fax:</td>
                 <td align="left" valign="top" class="border-top border-left border-right"><?php echo @$fax; ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-left">อีเมลล์ / Email :</td>
+                <td align="left" valign="top" class="border-left font-11">อีเมลล์ / Email :</td>
                 <td align="left" valign="top"> <?php echo @$contact_email; ?></td>
-                <td align="left" valign="top" class="border-left">อีเมล / Email :</td>
+                <td align="left" valign="top" class="border-left font-11">อีเมล / Email :</td>
                 <td align="left" valign="top" class="border-top border-left border-right"><?php echo @$email; ?></td>
             </tr>
             <tr>
-                <td align="left" valign="top" class="border-bottom border-top border-left">Term of Valid :</td>
+                <td align="left" valign="top" class="border-bottom border-top border-left font-11">ระยะเวลายืนราคา /
+                    Term of Valid :
+                </td>
                 <td align="left" valign="top" class="border-bottom border-top"> <?php echo @$term_of_valid; ?></td>
-                <td align="left" valign="top" class="border-bottom border-top border-left">การจ่ายเงิน / Pay of Valid
+                <td align="left" valign="top" class="border-bottom border-top border-left font-11">การจ่ายเงิน / Pay of
+                    Valid
                     :
                 </td>
                 <td align="left" valign="top"
                     class="border-bottom border-top border-right border-left"><?php echo @$pay_of_valid; ?></td>
             </tr>
-            <tr>
-                <td colspan="4">&nbsp;</td>
-            </tr>
-            <tr>
-                <td bgcolor="#FFC000" class="border">Term of payment :</td>
-                <td colspan="3" class="border-right border-top border-bottom"><?php echo @$term_of_payment; ?></td>
-            </tr>
-            <tr>
-                <td colspan="4">&nbsp;</td>
-            </tr>
+
         </table>
     </td>
 </tr>
+<tr>
+    <td>
+        <table width="100%" cellpadding="0" cellspacing="0">
+            <tr>
+                <td bgcolor="#FFC000" class="border font-11" width="160">Term of payment :</td>
+                <td class="border-right border-top border-bottom"><?php echo @$term_of_payment; ?></td>
+            </tr>
+        </table>
+    </td>
+<tr/>
 <tr><!-- quotation description info -->
     <td style="height: 450px !important; vertical-align: top;">
-        <table width="100%" cellpadding="0" cellspacing="0">
+        <table class="padding2px" width="100%" cellpadding="0" cellspacing="0">
             <col width="141">
             <col width="580">
             <col width="96">
@@ -220,36 +234,36 @@ $countQuotationItem = count($objQuotationItem)
                 </td>
             </tr>
             <?php if ($countQuotationItem): ?>
-            <?php for ($i = 0;
-                       $i < 12;
-                       $i++): ?>
+                <?php for ($i = 0;
+                           $i < 12;
+                           $i++): ?>
+                    <tr height="20">
+                        <td align="center"
+                            class="border-right border-left"><?php echo @$objQuotationItem[$i]->item_no; ?></td>
+                        <td class="border-right"><?php echo @$objQuotationItem[$i]->description; ?></td>
+                        <td height="31" align="right"
+                            class="border-right"><?php echo @$objQuotationItem[$i]->quantity > 0 ? @$objQuotationItem[$i]->quantity : "-&nbsp;&nbsp;"; ?></td>
+                        <td height="31" align="right"
+                            class="border-right"><?php echo @$objQuotationItem[$i]->price > 0 ? number_format(@$objQuotationItem[$i]->price, 2) : "-&nbsp;&nbsp;"; ?></td>
+                        <td height="31" align="right"
+                            class="border-right"><?php echo @$objQuotationItem[$i]->discount > 0 ? number_format(@$objQuotationItem[$i]->discount, 2) : "-&nbsp;&nbsp;"; ?></td>
+                        <td height="31" align="right"
+                            class="border-right"><?php echo @$objQuotationItem[$i]->amount > 0 ? number_format(@$objQuotationItem[$i]->amount, 2) : "-&nbsp;&nbsp;"; ?></td>
+                    </tr>
+                <?php endfor; ?>
                 <tr height="20">
                     <td align="center"
-                        class="border-right border-left"><?php echo @$objQuotationItem[$i]->item_no; ?></td>
-                    <td class="border-right"><?php echo @$objQuotationItem[$i]->description; ?></td>
+                        class="border-right border-left border-bottom"><?php echo @$objQuotationItem[12]->item_no; ?></td>
+                    <td class="border-right border-bottom"><?php echo @$objQuotationItem[12]->description; ?></td>
                     <td height="31" align="right"
-                        class="border-right"><?php echo @$objQuotationItem[$i]->quantity > 0 ? @$objQuotationItem[$i]->quantity : "-&nbsp;&nbsp;"; ?></td>
+                        class="border-right border-bottom"><?php echo @$objQuotationItem[12]->quantity > 0 ? @$objQuotationItem[12]->quantity : "-&nbsp;&nbsp;"; ?></td>
                     <td height="31" align="right"
-                        class="border-right"><?php echo @$objQuotationItem[$i]->price > 0 ? number_format(@$objQuotationItem[$i]->price, 2) : "-&nbsp;&nbsp;"; ?></td>
+                        class="border-right border-bottom"><?php echo @$objQuotationItem[12]->price > 0 ? number_format(@$objQuotationItem[12]->price, 2) : "-&nbsp;&nbsp;"; ?></td>
                     <td height="31" align="right"
-                        class="border-right"><?php echo @$objQuotationItem[$i]->discount > 0 ? number_format(@$objQuotationItem[$i]->discount, 2) : "-&nbsp;&nbsp;"; ?></td>
+                        class="border-right border-bottom"><?php echo @$objQuotationItem[12]->discount > 0 ? number_format(@$objQuotationItem[12]->discount, 2) : "-&nbsp;&nbsp;"; ?></td>
                     <td height="31" align="right"
-                        class="border-right"><?php echo @$objQuotationItem[$i]->amount > 0 ? number_format(@$objQuotationItem[$i]->amount, 2) : "-&nbsp;&nbsp;"; ?></td>
+                        class="border-right border-bottom"><?php echo @$objQuotationItem[12]->amount > 0 ? number_format(@$objQuotationItem[12]->amount, 2) : "-&nbsp;&nbsp;"; ?></td>
                 </tr>
-            <?php endfor; ?>
-            <tr height="20">
-                <td align="center"
-                    class="border-right border-left border-bottom"><?php echo @$objQuotationItem[12]->item_no; ?></td>
-                <td class="border-right border-bottom"><?php echo @$objQuotationItem[12]->description; ?></td>
-                <td height="31" align="right"
-                    class="border-right border-bottom"><?php echo @$objQuotationItem[12]->quantity > 0 ? @$objQuotationItem[12]->quantity : "-&nbsp;&nbsp;"; ?></td>
-                <td height="31" align="right"
-                    class="border-right border-bottom"><?php echo @$objQuotationItem[12]->price > 0 ? number_format(@$objQuotationItem[12]->price, 2) : "-&nbsp;&nbsp;"; ?></td>
-                <td height="31" align="right"
-                    class="border-right border-bottom"><?php echo @$objQuotationItem[12]->discount > 0 ? number_format(@$objQuotationItem[12]->discount, 2) : "-&nbsp;&nbsp;"; ?></td>
-                <td height="31" align="right"
-                    class="border-right border-bottom"><?php echo @$objQuotationItem[12]->amount > 0 ? number_format(@$objQuotationItem[12]->amount, 2) : "-&nbsp;&nbsp;"; ?></td>
-            </tr>
             <?php endif; ?>
         </table>
     </td>
@@ -263,7 +277,7 @@ $countQuotationItem = count($objQuotationItem)
             <col width="91" span="2">
             <col width="92">
             <tr>
-                <td width="120" align="left" valign="top">Note :</td>
+                <td width="98" align="left" valign="top">Note :</td>
                 <td align="left" valign="top"><?php echo nl2br(@$remark); ?></td>
                 <td width="225" height="20" align="left" bgcolor="#FFC000" class="border-top border-right border-left">
                     Discount
@@ -273,19 +287,19 @@ $countQuotationItem = count($objQuotationItem)
                     class="border-top border-right"><?php echo number_format(@$discount_total, 2); ?></td>
             </tr>
             <tr>
-                <td width="120" align="left" valign="top">&nbsp;</td>
+                <td width="98" align="left" valign="top">&nbsp;</td>
                 <td align="left" valign="top">&nbsp;</td>
                 <td height="20" align="left" bgcolor="#FFC000" class="border-top border-right border-left">Amount</td>
                 <td align="right" class="border-top border-right"><?php echo number_format(@$amount, 2); ?></td>
             </tr>
             <tr>
-                <td width="120" align="left" valign="top">&nbsp;</td>
+                <td width="98" align="left" valign="top">&nbsp;</td>
                 <td align="left" valign="top">&nbsp;</td>
                 <td height="20" align="left" bgcolor="#FFC000" class="border-top border-right border-left">Vat 7%</td>
                 <td align="right" class="border-top border-right"><?php echo number_format(@$vat, 2); ?></td>
             </tr>
             <tr>
-                <td width="120" align="left" valign="top">&nbsp;</td>
+                <td width="98" align="left" valign="top">&nbsp;</td>
                 <td align="left" valign="top">&nbsp;</td>
                 <td height="20" align="left" bgcolor="#FFC000"
                     class="border-top border-right border-left border-bottom">Total Amount
@@ -294,18 +308,18 @@ $countQuotationItem = count($objQuotationItem)
                     class="border-top border-right border-bottom"><?php echo number_format(@$total_amount, 2); ?></td>
             </tr>
             <tr>
-                <td width="120" align="left" valign="top">&nbsp;</td>
+                <td width="98" align="left" valign="top">&nbsp;</td>
                 <td align="left" valign="top">&nbsp;</td>
                 <td height="15" colspan="2" align="right">&nbsp;</td>
             </tr>
             <tr>
-                <td width="120" align="left" valign="top">&nbsp;</td>
+                <td width="98" align="left" valign="top">&nbsp;</td>
                 <td align="left" valign="top">&nbsp;</td>
                 <td height="20" colspan="2" align="center" bgcolor="#FFC000"
                     class="border"><?php echo @$total_amount_text; ?></td>
             </tr>
             <tr>
-                <td width="120" align="left" valign="top">&nbsp;</td>
+                <td width="98" align="left" valign="top">&nbsp;</td>
                 <td align="left" valign="top">&nbsp;</td>
                 <td height="15" colspan="2" align="right">&nbsp;</td>
             </tr>
@@ -328,11 +342,16 @@ $countQuotationItem = count($objQuotationItem)
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;)
                 </td>
-                <td width="300" align="left" valign="top" ><p>Present By
+                <td width="300" align="left" valign="top"><p>Present By
                     </p>
                     <?php
-                    echo file_exists(@$signature_path) ?
-                        "<img style='margin-top:-65px;position:absolute;margin-left:80px;z-index: -1;' width='200' src='" . $baseUrl . $signature_path . "'/>" : "";
+                    if (file_exists(@$signature_path)):
+                        ?>
+                        <img style="margin-top:-65px;position:absolute;margin-left:80px;z-index: -1;"
+                             width="200"
+                             src="<?php echo $baseUrl . $signature_path; ?>"/>
+                    <?php
+                    endif;
                     ?>
                     <p align="center">_________________________________________________</p>
 
