ceb7fc9dca9d49995ceafe527d2bac4f36596daf
Rux <ruxchuk@gmail.com>
2014-09-17 10:13:49 +0700

    edit bug

--- application/views/project/list.php
+++ application/views/project/list.php
@@ -76,7 +76,7 @@ $objData = $this->Project_model->projectList();
                                                     } else {
                                                         echo '-';
                                                     } ?></td>
-                                                <td><?php echo $value->customer_name_th ? $value->customer_name_th : $value->compnay_name_en; ?></td>
+                                                <td><?php echo $value->customer_name_th ? $value->customer_name_th : $value->customer_name_en; ?></td>
                                                 <td><?php echo $value->project_start; ?></td>
                                                 <td><?php echo "$value->project_end"; ?></td>
                                                 <td><?php echo $value->update_datetime; ?></td>
